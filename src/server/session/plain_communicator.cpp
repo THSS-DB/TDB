96ca7509e8ba3be566c38da67b466796345caa05
YangCaiyin <ycycse@gmail.com>
2024-03-07 17:57:31 +0800

    bug:missing end message in TCP client

--- src/server/session/plain_communicator.cpp
+++ src/server/session/plain_communicator.cpp
@@ -92,6 +92,8 @@ RC PlainCommunicator::write_state(SqlResult *sql_result, bool &need_disconnect)
     return RC::IOERR_WRITE;
   }
 
+  send_message_delimiter();
+
   need_disconnect = false;
   delete[] buf;
 
