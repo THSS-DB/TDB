845291a510a2bfced95692b55bb1d71f056cd747
YangCaiyin <ycycse@gmail.com>
2024-04-09 11:26:35 +0800

    feat: support load from file

--- src/server/query_engine/executor/load_data_executor.cpp
+++ src/server/query_engine/executor/load_data_executor.cpp
@@ -122,7 +122,8 @@ void LoadDataExecutor::load_data(Table *table, const char *file_name, SqlResult 
   struct timespec begin_time;
   clock_gettime(CLOCK_MONOTONIC, &begin_time);
   const int sys_field_num = table->table_meta().sys_field_num();
-  const int field_num = table->table_meta().field_num() - sys_field_num;
+  const int null_field_num = table->table_meta().null_filed_num();
+  const int field_num = table->table_meta().field_num() - sys_field_num - null_field_num;
 
   std::vector<Value> record_values(field_num);
   std::string line;
